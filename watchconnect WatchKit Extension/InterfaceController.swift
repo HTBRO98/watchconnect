930784808e082978361e3ca99ccea93d998033da
HTBRO98 <srty.mv.fu@gmail.com>
2021-06-12 21:08:12 +0900

    Initial Commit

---
+++ watchconnect WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  watchconnect WatchKit Extension
+//
+//  Created by HAYATOYAMAMOTo on 2021/06/12.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
